e770d2de6d8d7b7b7ea58a9a5b0ef602228c6153
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-01 12:15:35 -0500

    checking for errors

--- models/place.py
+++ models/place.py
@@ -23,7 +23,6 @@ class Place(BaseModel, Base):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "places"
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
