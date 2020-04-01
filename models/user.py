29dff2f41f078c2918b6da24d2b7c10abf8415dc
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-01 10:07:49 -0500

    nullable in first name and last name set to True

--- models/user.py
+++ models/user.py
@@ -20,8 +20,8 @@ class User(BaseModel, Base):
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
     elif getenv('HBNB_TYPE_STORAGE') == 'file':
         email = ''
         password = ''
