9c15a32901710cd1e8e5b469eb7c762c00b80e86
Jormao <jormao@gmail.com>
2020-04-01 02:23:36 +0000

    change the storage engine and use SQLAlchemy

--- models/state.py
+++ models/state.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    elif getenv('HBNB_TYPE_STORAGE') == 'file':
+        name = ''
+
+        @property
+        def cities(self):
+            """Return a list of City instance
+            """
+            city_list = []
+            for _id, city in models.storage.all(City).items():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
