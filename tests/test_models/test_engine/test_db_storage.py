9a7b9a90151cb7337559d278a932f64e64379611
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-02 16:38:51 -0500

    unittests for the db_storage engine

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-"""test for file storage"""
+#!/usr/bin/python
+"""Unittests for DBStorage class of AirBnb_Clone_v2"""
 import unittest
 import pep8
-import json
 import os
+from os import getenv
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -11,23 +11,87 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
+import MySQLdb
 
 
-@unittest.skipIf(
-    os.getenv('HBNB_TYPE_STORAGE') != 'db', "Should be work in db")
-class TestDbStorage(unittest.TestCase):
-    '''this will test the DbStorage'''
+class TestDBStorage(unittest.TestCase):
+    '''this will test the DBStorage'''
 
-    def testState(self):
-        state = State(name="Antioquia")
-        if state.id in models.storage.all():
-            self.asserTrue(state.name, "Antioquia")
+    @classmethod
+    def setUpClass(cls):
+        """Tests"""
+        cls.user = User()
+        cls.user.first_name = "Kev"
+        cls.user.last_name = "Yo"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = FileStorage()
 
-    def testCity(self):
-        state = State(name="Medellin")
-        if city.id in models.storage.all():
-            self.asserTrue(state.name, "Medellin")
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
+
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_DBStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_all(self):
+        """tests if all works in DB Storage"""
+        storage = FileStorage()
+        obj = storage.all()
+        self.assertIsNotNone(obj)
+        self.assertEqual(type(obj), dict)
+        self.assertIs(obj, storage._FileStorage__objects)
+
+    def test_new(self):
+            """test when new is created"""
+            storage = FileStorage()
+            obj = storage.all()
+            user = User()
+            user.id = 123455
+            user.name = "Kevin"
+            storage.new(user)
+            key = user.__class__.__name__ + "." + str(user.id)
+            self.assertIsNotNone(obj[key])
+
+    def test_reload_dbtorage(self):
+            """
+            tests reload
+            """
+            self.storage.save()
+            Root = os.path.dirname(os.path.abspath("console.py"))
+            path = os.path.join(Root, "file.json")
+            with open(path, 'r') as f:
+                lines = f.readlines()
+            try:
+                os.remove(path)
+            except Exception:
+                pass
+            self.storage.save()
+            with open(path, 'r') as f:
+                lines2 = f.readlines()
+            self.assertEqual(lines, lines2)
+            try:
+                os.remove(path)
+            except Exception:
+                pass
+            with open(path, "w") as f:
+                f.write("{}")
+            with open(path, "r") as r:
+                for line in r:
+                    self.assertEqual(line, "{}")
+            self.assertIs(self.storage.reload(), None)
 
 
 if __name__ == "__main__":
