9a870978a0364f565df629ce5cd9eb10f283ebdd
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-02 16:46:45 -0500

    unittests for the db_storage engine

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -16,6 +16,9 @@ from models.engine.file_storage import FileStorage
 import MySQLdb
 
 
+@unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') != 'db',
+        "This test only work in DBStorage")
 class TestDBStorage(unittest.TestCase):
     '''this will test the DBStorage'''
 
