19c950f1ecd3fcef62b200131328aeae734e1675
Jormao <jormao@gmail.com>
2020-04-02 00:26:33 +0000

    fix amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -16,5 +16,5 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary=Place.place_amenity)
+        place_amenities = relationship('Place', secondary='place_amenity')
     name = ""
