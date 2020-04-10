8d4ba228c45481d2959b4f322db860b48ffb66c8
jormao <jormao@gmail.com>
2020-04-09 20:18:05 -0500

    fix /hbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,8 +3,8 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "Holberton School fake html" | sudo tee /data/web_static/releases/test/index.html
+echo -e "<html>\n <head>\n </head>\n <body>\n Holberton School\n </body>\n/<html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo sed -i '42i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
