42dbd81dc71ae12fe10bf1a6574f41e65744028b
jormao <jormao@gmail.com>
2020-04-12 14:18:52 -0500

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,27 +3,10 @@
 """
 from fabric.operations import local, run, put
 from datetime import datetime
-from fabric.api import env
+from fabric.api import *
 from os import path
-env.user = 'ubuntu'
-env.hosts = [
-    '35.229.126.169',
-    '54.90.172.156',
-]
 
-
-def do_pack():
-    """archive from the contents of the web_static folder
-    using do_pack function
-    """
-    local("mkdir -p versions")
-    try:
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(
-            datetime.now().strftime("%Y%m%d%H%M%S")))
-        return "versions/web_static_{}.tgz web_static/".format(
-            datetime.now().strftime("%Y%m%d%H%M%S"))
-    except Exception:
-        return None
+env.hosts = ['35.229.126.169', '54.90.172.156']
 
 
 def do_deploy(archive_path):
@@ -32,17 +15,17 @@ def do_deploy(archive_path):
     if not path.exists(archive_path):
         return False
     try:
-        name_ext = archive_path.split("/")
-        only_name = name_ext[-1].split(".")
-        new_path = "/data/web_static/releases/" + only_name[0]
+        name_ext = archive_path.split("/")[-1]
+        only_name = name_ext.split(".")[0]
+        new_path = "/data/web_static/releases/" + only_name + "/"
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(new_path))
-        run("sudo tar -xzf /tmp/{} -C {}".format(name_ext[-1], new_path))
-        run("sudo rm /tmp/{}".format(name_ext[1]))
+        run("sudo tar -xzf /tmp/{} -C {}".format(name_ext, new_path))
+        run("sudo rm /tmp/{}".format(name_ext))
         run("sudo mv {}/web_static/* {}/".format(new_path, new_path))
         run("sudo rm -rf {}/web_static".format(new_path))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -sf {} /data/web_static/current".format(new_path))
+        run("sudo ln -s {} /data/web_static/current".format(new_path))
         print("New version deployed!")
         return True
     except Exception:
