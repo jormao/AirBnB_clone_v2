3c947636c8500a94b9771cf6fbad1cdba93e1312
jormao <jormao@gmail.com>
2020-04-21 01:25:43 -0500

    fix comments

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application:
 web application must be listening on 0.0.0.0, port 5000
-Routes: /: display “Hello HBNB!”
+Routes: 
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
 must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
